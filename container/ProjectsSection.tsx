b7548d69dac409cbcca9516fe1d496fa3e076afb
Atharvashuks <71465575+Atharvashuks@users.noreply.github.com>
2024-08-23 01:56:08 +0530

    removed unwanted projects (#6)

--- container/ProjectsSection.tsx
+++ container/ProjectsSection.tsx
@@ -25,15 +25,6 @@ const ProjectSection = [
 const projectsData = [
   {
     id: 1,
-    title: "React Portfolio Website",
-    description: "Project 1 description",
-    image: "/images/projects/1.png",
-    tag: ["All", "Web"],
-    gitUrl: "/",
-    previewUrl: "/",
-  },
-  {
-    id: 2,
     title: "Potography Portfolio Website",
     description: "Project 2 description",
     image: "/images/projects/2.png",
@@ -42,7 +33,7 @@ const projectsData = [
     previewUrl: "/",
   },
   {
-    id: 3,
+    id: 2,
     title: "E-commerce Application",
     description: "Project 3 description",
     image: "/images/projects/3.png",
@@ -51,7 +42,7 @@ const projectsData = [
     previewUrl: "/",
   },
   {
-    id: 4,
+    id: 3,
     title: "Food Ordering Application",
     description: "Project 4 description",
     image: "/images/projects/4.png",
@@ -60,19 +51,10 @@ const projectsData = [
     previewUrl: "/",
   },
   {
-    id: 5,
+    id: 4,
     title: "React Firebase Template",
     description: "Authentication and CRUD operations",
     image: "/images/projects/5.png",
-    tag: ["All", "Web"],
-    gitUrl: "/",
-    previewUrl: "/",
-  },
-  {
-    id: 6,
-    title: "Full-stack Roadmap",
-    description: "Project 5 description",
-    image: "/images/projects/6.png",
     tag: ["All", "Web"],
     gitUrl: "/",
     previewUrl: "/",
