1d4e92a16cbfce20af5608e7e03f03a1c00eb353
Atharvashuks <71465575+Atharvashuks@users.noreply.github.com>
2025-12-16 15:50:30 +0530

    Update route.ts

--- app/api/data/About/POST/route.ts
+++ app/api/data/About/POST/route.ts
@@ -16,7 +16,7 @@ import { saveDataToDB } from "../../../../../Utility/postDataUtility";
 import prisma from "@/config/db";
 import { authGuard } from "../../../../middleware/authGuard";
 
-export const POST = authGuard(async (req: Request) => {
+export const POST = authGuard(async (req) => {
   const extractData = await req.json();
 
   const response = await saveDataToDB(prisma.about, {
